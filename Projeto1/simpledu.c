a0c0f2cdd69e3229288ad4dcb23da5528225a68f
BernardoFerreira00 <56405746+BernardoFerreira00@users.noreply.github.com>
2020-03-28 21:48:18 +0000

    some changes

--- Projeto1/simpledu.c
+++ Projeto1/simpledu.c
@@ -2,44 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-int validOption(char options) {
+#define true 1
+#define false 0
 
+int validOption(char *option) {
 
+    if(!strcmp(option, "-a") || !strcmp(option, "-b") || !strcmp(option, "-B") 
+        || !strcmp(option, "-L") || !strcmp(option, "-S") || !strcmp(option, "-N")) {
+            return true;
+    }
+    return false;
 }
-
-
-
 
 
 int main(int argc, char *argv[], char *envp[]) {
 
-    char path[100], *options[8];
+    char dirName[100], *options[8];
     int j = 0;
 
-    if(argc < 2 || argc > 10 || (argv[2] != "-l" || argv[2] != "- count-links")) {
+    
+    if(argc < 2 || strcmp(argv[1],"-l")) {
         printf("Invalid Arguments!\n");
+        exit(1);
     }
 
     if(argc == 2) {
         //em desenvolvimento
     }
 
-    for(int i=0; i<argc; i++) {
+    for(int i=2; i<argc; i++) {
         if(argv[i][0] != '-') {
-            strcpy(path,argv[i]);
-            printf("%s\n",path);
+            strcpy(dirName,argv[i]);
+            printf("%s\n",dirName);
         }
         else {
             options[j] = argv[i];
+            if(!validOption(options[j])) {
+                printf("%s is a invalid option\n", options[j]);
+                exit(2);
+            }
             j++;
         }
-
-
     }
     
-    
-    for(int i=0; i<=argc-4; i++) {
+    for(int i=0; i<=argc-4; i++) { //So para questoes de teste
         printf("%s\n",options[i]);
     }
+
+    return 0;
 
 }
