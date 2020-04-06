ec9c3b28cdf7e940155dcc05099c1be67942492c
JayRx <37720991+JayRx@users.noreply.github.com>
2020-04-06 17:55:32 +0100

    Updated list_contents and added listContentsPrint void
    - Updated list_contents from simpledu.c
    - Added listContentsPrint void to simpledu.c
    
    Now the program prints the same as du when called with or without -a and -L. Can't yet print content from a subdirectory of a subdirectory of the main directory that we want info from.

--- Projeto1/simpledu.c
+++ Projeto1/simpledu.c
@@ -32,46 +32,61 @@ void printDir(char *dirName) {
   fprintf(stderr, "Directory: ./%s\n", dirName);
 }
 
+void listContentsPrint(char *dirName, struct dirent *dentry, struct stat stat_entry) {
+  if(strcmp(dentry->d_name, ".") == 0) {}
+  else if(strcmp(dentry->d_name, "..") == 0) {}
+  else
+    printf("%d\t%s/%-25s\n", (int)stat_entry.st_size, dirName, dentry->d_name);
+}
+
 //Lista os Ficheiros Regulares (para ja)
 int list_contents(char *dirName, char *options[]) {
   struct dirent *dentry;
   struct stat stat_entry;
   DIR *dir;
+  int main_dir_size = 0;
 
   if((dir = opendir(dirName)) == NULL) {
       perror("Directory Error");
   }
 
   chdir(dirName);
-  fprintf(stderr, "Files of ");
-  printDir(dirName);
 
     while((dentry = readdir(dir)) != NULL) {
-        
-        if(checkPresenceOfOption("L", options) 
-            || checkPresenceOfOption("dereference", options)) {
-            lstat(dentry->d_name, &stat_entry);
-        }else {
-            stat(dentry->d_name, &stat_entry);
+        stat(dentry->d_name, &stat_entry);
+
+        if (strcmp(dentry->d_name, ".") == 0){
+          main_dir_size = (int)stat_entry.st_size;
         }
 
-        if(checkPresenceOfOption("a", options) || checkPresenceOfOption("all",options)) {
+        if((checkPresenceOfOption("L", options)
+            || checkPresenceOfOption("dereference", options)) && (checkPresenceOfOption("a", options)
+            || checkPresenceOfOption("all", options))) {
             if (S_ISREG(stat_entry.st_mode)) {
-                printf("%d\t%s/%-25s\n", (int)stat_entry.st_size, dirName, dentry->d_name);
+              listContentsPrint(dirName, dentry, stat_entry);
+            } else if (S_ISDIR(stat_entry.st_mode)) {
+              listContentsPrint(dirName, dentry, stat_entry);
+            }  else if (S_ISLNK(stat_entry.st_mode)) {
+              listContentsPrint(dirName, dentry, stat_entry);
             }
-            if(S_ISLNK(stat_entry.st_mode)) { //Fazer assim nao sei se e melhor opçao(a unica que eu vi)
-                printf("%d\t%s/%-25s\n", (int)stat_entry.st_size, dirName, dentry->d_name);
+        } else if (checkPresenceOfOption("a", options) || checkPresenceOfOption("all", options)){
+            if (S_ISREG(stat_entry.st_mode)) {
+              listContentsPrint(dirName, dentry, stat_entry);
+            } else if (S_ISDIR(stat_entry.st_mode)) {
+              listContentsPrint(dirName, dentry, stat_entry);
             }
-        }else { //Isto nao esta bem era suposto imprimir um subdiretorio e imprimi o diretorio passado varias vezes
+        } else {
             if(S_ISDIR(stat_entry.st_mode)) {
-                printf("%d\t%s\n", (int)stat_entry.st_size, dirName);
-
+                listContentsPrint(dirName, dentry, stat_entry);
             }
         }
     }
+
+    printf("%d\t%s\n", main_dir_size, dirName);
+
     return 0;
 }
-    
+
 int main(int argc, char *argv[], char *envp[]) {
 
     char dirName[100], *options[8], maxDepth[15], blockSize[15], bSize[15], forCheck[25];
@@ -80,7 +95,7 @@ int main(int argc, char *argv[], char *envp[]) {
     for(int i=0; i<8; i++) {
         *(options+i) = (char*) malloc(15*sizeof(char));
     }
-    
+
     if (argc < 2 || (strcmp(argv[1], "-l") && strcmp(argv[1], "--cout-links"))) { //Verifica se o utilizador degita a opçao -l
         invalidArgs(argv);
     }
@@ -91,7 +106,6 @@ int main(int argc, char *argv[], char *envp[]) {
     for (int i = 2; i < argc; i++){
         if (argv[i][0] != '-'){ // Adiciona o diretorio introduzido pelo utilizador a variavel dirName
             strcpy(dirName, argv[i]);
-            printDir(dirName);
         }
         if (argv[i][0] == '-' && argv[i][1] != '-'){ //Adiciona ao options as opçoes do tipo -(nome)
             if(argv[i][1] == 'B') {
@@ -129,17 +143,17 @@ int main(int argc, char *argv[], char *envp[]) {
             }
             j++;
         }
-       
+
     }
-    for (int i = 0; i < argc - 3; i++) { //So para questoes de teste
+    /*for (int i = 0; i < argc - 3; i++) { //So para questoes de teste
         printf("%s-->%d\n", options[i], i);
-    }
-    
+    }*/
+
     if(checkRepeatedElements(options, argc-3) == true) {
         printf("There is repeated elements\n");
         exit(6);
     }
-    
+
     list_contents(dirName, options);
 
     return 0;
