89dac4f2e853ee68f7d43c50878b5575458d4efa
BernardoFerreira00 <56405746+BernardoFerreira00@users.noreply.github.com>
2020-04-10 12:19:19 +0100

    some changes and bug fixes

---
+++ directory.h
@@ -0,0 +1,22 @@
+#ifndef DIRECTORY_H_
+#define DIRECTORY_H_
+
+#define DEV_BSIZE 512
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define true 1
+#define false 0
+
+void printDir(char *dirName);
+void listContentsPrint(char *dirName, struct dirent *dentry, struct stat stat_entry, int hasb, int b_size);
+int list_contents(char *dirName, char *options[], int b_size, int m_depth, char *argv[], int argc);
+int funcToFork(char *dirName, char *options[], int m_depth, int argc, char *args[], struct dirent *dentry);
+
+#endif // DIRECTORY_H_
