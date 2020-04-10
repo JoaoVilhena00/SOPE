4b3f429b630b2a1a3740497f75f62d4112f06d85
JayRx <37720991+JayRx@users.noreply.github.com>
2020-04-10 20:21:22 +0100

    Bug Fix

--- Projeto1/directory.c
+++ Projeto1/directory.c
@@ -45,8 +45,6 @@ int list_contents(char *dirName, char *options[], int b_size, int m_depth, char 
 
   args[argc] = (char *) NULL;
 
-  printf("=====%s\n",dirName);
-
   if((dir = opendir(dirName)) == NULL) {
       perror("Directory Error");
   }
@@ -60,9 +58,9 @@ int list_contents(char *dirName, char *options[], int b_size, int m_depth, char 
    }*/
 
     while((dentry = readdir(dir)) != NULL) {
-        //sprintf(name,"%s/%s",dirName,dentry->d_name);
-        stat(name, &stat_entry);
-        
+        sprintf(name2,"%s/%s",dirName,dentry->d_name);
+        stat(name2, &stat_entry);
+
         if(checkPresenceOfOption("b",options) || checkPresenceOfOption("bytes",options)) {
             hasb = true;
         }else {
@@ -73,22 +71,22 @@ int list_contents(char *dirName, char *options[], int b_size, int m_depth, char 
           main_dir_size += (int)stat_entry.st_size;
         } else if(hasb == false)
             main_dir_size += (int)stat_entry.st_blocks / 2;
-        
-        
+
+
 
         if(S_ISDIR(stat_entry.st_mode) && strcmp(dentry->d_name,".") && strcmp(dentry->d_name,"..")) {
           strcpy(name, dirName);
           strcat(name, "/");
           strcat(name, dentry->d_name);
           strcpy(args[2], name);
-          
+
           if (checkPresenceOfOption("max-depth", options)) {
             m_depthAux = m_depth - 1;
             sprintf(args[argc-1], "--max-depth=%d", m_depthAux);
           }
-          
+
           pid = fork();
-          
+
           if(pid > 0) {
             childpid = wait(&status);
           } else if(pid == 0 ) {
