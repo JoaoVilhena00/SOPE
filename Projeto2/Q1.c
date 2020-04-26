28eaff1a2c4670457164f45acbd6aaefe6345d6b
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-04-26 18:52:59 +0100

    reads the arguments

--- Projeto2/Q1.c
+++ Projeto2/Q1.c
@@ -2,18 +2,39 @@
 #include "stdlib.h"
 #include "string.h"
 #include <pthread.h>
+#include "auxiliary.h"
 
-void get_options(int argc, char *argv[], char *options[], int *nsecs, char *fifoname) {
+void get_options(int argc, char *argv[], char *options[], int *nsecs, int *nplaces, int *nthreads, char *fifoname) {
 
+    int j=0;
 
-    for(int i=0; i<) {
-
-
+    if(argc < 5) {
+        //print error
     }
 
-
-
-
+    for(int i=1; i<argc; i++) {
+        strcpy(options[j], argv[i]);
+        if(strcmp(argv[i], "-t") == 0){
+            i++;
+            if(i >= argc)
+               //print error
+            sscanf(argv[i], "%d", nsecs);
+        }else if(strcmp(argv[i], "-l") == 0) {
+            i++;
+            if(i >= argc) {
+                //print error
+            }
+            sscanf(argv[i], "%d", nplaces);
+        }else if(strcmp(argv[i], "-l") == 0) {
+            i++;
+            if(i >= argc) {
+                //print error
+            }
+            sscanf(argv[i], "%d", nthreads);
+        } else {
+            strcpy(fifoname, argv[i]);
+        }
+    }
 }
 
 
