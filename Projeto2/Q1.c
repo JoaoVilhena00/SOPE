4b79f60a22e2c8e2bedf80e4d1ae0a22675c8f5b
BernardoFerreira00 <bernardoferreira2013@gmail.com>
2020-04-26 18:32:08 +0100

    added Q1 file

---
+++ Projeto2/Q1.c
@@ -0,0 +1,36 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include <pthread.h>
+
+void get_options(int argc, char *argv[], char *options[], int *nsecs, char *fifoname) {
+
+
+    for(int i=0; i<) {
+
+
+    }
+
+
+
+
+}
+
+
+
+
+int main(int argc, char *argv[]) {
+  char *options[8];
+  char fifoname[15];
+  int nsecs = -1;
+
+  //print_argv(argc, argv);
+
+  for (int i = 0; i < 8; i++) {
+    *(options + i) = (char *) malloc(15 * sizeof(char));
+  }
+
+  
+
+  return 0;
+}
