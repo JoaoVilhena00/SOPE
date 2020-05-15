8a1a1c01ed94e05f49723c0d8c93817718deb6e8
BernardoFerreira00 <56405746+BernardoFerreira00@users.noreply.github.com>
2020-05-15 14:20:30 +0100

    Add files via upload

---
+++ Projeto2/auxiliary.c
@@ -0,0 +1,10 @@
+#include "auxiliary.h"
+
+void regist_message(int i, pid_t pid, pid_t tid, int dur, int pl, char *oper) {
+    char* message;
+    message = (char*) malloc (128 * sizeof(char));
+    time_t t = time(NULL);
+
+    sprintf(message, "%ld ; %d ; %d ; %d ; %d ; %d ; %s\n", t, i, pid, tid, dur, pl, oper);
+    write(STDOUT_FILENO, message, strlen(message));
+}
